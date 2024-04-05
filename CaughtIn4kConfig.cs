225610d4cc94b065b6a98d094aa2fad9150351f0
SlenLen <68658538+SlenLen@users.noreply.github.com>
2024-04-05 05:33:46 +0200

    Update CaughtIn4kConfig.cs
    fix typo and clarify better

--- CaughtIn4kConfig.cs
+++ CaughtIn4kConfig.cs
@@ -11,8 +11,8 @@ internal class CaughtIn4KConfig
     internal ConfigEntry<int> RESOLUTION_Y; // vertical resolution
 
     internal CaughtIn4KConfig(ConfigFile cfgFile) {
-        RESOLUTION_X = cfgFile.Bind<int>("Main", "ResolutionX", 840, new ConfigDescription("The Video Camera's Horizontal Resolution\nIDK how well aspect ratios other than 1:1 work, so I don't recommend setting the side lengths to different values."));
-        RESOLUTION_Y = cfgFile.Bind<int>("Main", "ResolutionY", 840, new ConfigDescription("The Video Camera's Vertical Resolution\nDefault values are 2x the vanilla resolution. Be aware that raising the resoltion increases the video file size very quickly."));
+        RESOLUTION_X = cfgFile.Bind<int>("Main", "ResolutionX", 840, new ConfigDescription("The Video Camera's Horizontal Resolution\nDifferent aspect ratios than the vanilla one (1:1) actually seem to work reasonably fine."));
+        RESOLUTION_Y = cfgFile.Bind<int>("Main", "ResolutionY", 840, new ConfigDescription("The Video Camera's Vertical Resolution\nDefault values are 2x the vanilla resolution. Be that raising the total resolution (x*y) too high can cause huge file sizes, CPU utilization and RAM Usage."));
     }
 
 }
